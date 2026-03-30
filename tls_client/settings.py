b695e5ea56ba07b155236476eb112ab2684d018e
Nintendocustom <nintendocustomyt@gmail.com>
2026-03-30 23:49:30 +0200

    add Brave profiles

--- tls_client/settings.py
+++ tls_client/settings.py
@@ -27,6 +27,8 @@ ClientIdentifiers: TypeAlias = Literal[
     "chrome_144_PSK",
     "chrome_146",
     "chrome_146_PSK",
+    "brave_146",
+    "brave_146_PSK",
     # Safari
     "safari_15_6_1",
     "safari_16_0",
